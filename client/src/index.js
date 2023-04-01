331a92db5def07a11e7735efce457ceb2041f81d
NoeGJ <114208557+NoeGJ@users.noreply.github.com>
2023-03-31 21:10:27 -0600

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
+import App from './routes/App';
+import Add_user from './routes/add_user';
+import ErrorPage from './routes/error-page';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "new",
+    element: <Add_user/>,
+  },
+
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
