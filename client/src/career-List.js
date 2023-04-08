ab89f4e4178936ae10521754f6116311518e8835
NoeGJ <114208557+NoeGJ@users.noreply.github.com>
2023-04-08 17:18:45 -0600

    Add files via upload

---
+++ client/src/career-List.js
@@ -0,0 +1,95 @@
+const carreras = [
+    {    
+        key: 1,
+        name: 'Ingenieria informatica',
+        value: 'INNI',
+    },
+    {    
+        key: 2,
+        name: 'Ingenieria en computacion',
+        value: 'INCO',
+    },
+    {    
+        key: 3,
+        name: 'Ingenieria en comunicaciones y electronica',
+        value: 'INCE',
+    },
+    {    
+        key: 4,
+        name: 'Ingenieria en biomedica',
+        value: 'INBI',
+    },
+    {    
+        key: 5,
+        name: 'Ingenieria fotonica',
+        value: 'IGFO',
+    },
+    {    
+        key: 6,
+        name: 'Ingenieria robotica',
+        value: 'INRO',
+    },
+    {    
+        key: 7,
+        name: 'Ingenieria civil',
+        value: 'ICIV',
+    },
+    {    
+        key: 8,
+        name: 'Ingenieria en alimentos y biotecnologia',
+        value: 'INAB',
+    },
+    {    
+        key: 9,
+        name: 'Ingenieria en topografia geomatica',
+        value: 'ITOG',
+    },
+    {    
+        key: 10,
+        name: 'Ingenieria industrial',
+        value: 'INDU',
+    },
+    {    
+        key: 11,
+        name: 'Ingenieria mecanica electronica',
+        value: 'INME',
+    },
+    {    
+        key: 12,
+        name: 'Ingenieria quimica',
+        value: 'INQU',
+    },
+    {    
+        key: 13,
+        name: 'Ingenieria en logistica y transporte',
+        value: 'ILOT',
+    },
+    {    
+        key: 14,
+        name: 'Licenciatura en fisica',
+        value: 'LIMA',
+    },
+    {    
+        key: 15,
+        name: 'Licenciatura en matematicas',
+        value: 'LIFI',
+    },
+    {    
+        key: 16,
+        name: 'Licenciatura en quimica',
+        value: 'LQUI',
+    },
+    {    
+        key: 17,
+        name: 'Quimico farmaceutico biologo',
+        value: 'LQFB',
+    },
+    {    
+        key: 18,
+        name: 'Licenciatura en ciencia de materiales',
+        value: 'LCMA',
+    },
+
+];
+
+export default carreras;
