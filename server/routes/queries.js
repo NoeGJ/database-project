6005a72bd731846d42cd79f2df2e0913b6e25f0a
NoeGJ <114208557+NoeGJ@users.noreply.github.com>
2023-05-03 00:14:59 -0600

    Add files via upload

--- server/routes/queries.js
+++ server/routes/queries.js
@@ -24,7 +24,7 @@ const getNombreModalidades = (req, res) => {
 
 const createStudent = (req, res) => {
     const [ estatus, student  ] = req.body;
-    const { name, folio, age, genre, code, career, ingreso, egreso } = student
+    const { name, folio, age, gender, code, career, ingreso, egreso } = student
     const { graduated, startDate, endDate, average, type } = estatus
     
     console.log(type)
@@ -35,7 +35,7 @@ const createStudent = (req, res) => {
     .then( () => db.one('INSERT INTO estatus (titulado, inicio_titulacion, fin_titulacion, calificacion, id_mod1) VALUES ($1, $2, $3, $4, $5) returning id_estatus', 
       [graduated, startDate, endDate, average, id_mod])
         .then( ( { id_estatus } ) => db.none('INSERT INTO alumno (codigo, folio, nombre, sexo, edad, carrera, ciclo_ingreso, ciclo_egreso, id_estatus1) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)', 
-        [code, folio, name,  genre, age , career, ingreso, egreso, id_estatus])
+        [code, folio, name,  gender, age , career, ingreso, egreso, id_estatus])
         .then(() => {
          console.log("success");
          res.status(201).json({
@@ -54,14 +54,14 @@ const createStudent = (req, res) => {
 
 const updateStudent = ( req, res ) => {
     const [ estatus, student ] = req.body;
-    const { name, folio, age, genre, code, career, ingreso, egreso } = student
+    const { name, folio, age, gender, code, career, ingreso, egreso } = student
     const { graduated, startDate, endDate, average, type } = estatus
     
     let  mod;
     db.any('SELECT id_mod from modalidad_titulacion WHERE nombre = $1 ', [ type ])
     .then( ( { id_mod } ) => mod = id_mod  )
     .then( () => db.one('UPDATE alumno SET folio = $1, nombre = $2, sexo = $3, edad = $4, carrera = $5, ciclo_ingreso = $6, ciclo_egreso = $7 WHERE codigo = $8 RETURNING id_estatus1',
-    [folio, name, genre, age, career, ingreso, egreso, code]) )
+    [folio, name, gender, age, career, ingreso, egreso, code]) )
     .then( ( { id_estatus1 } ) => db.none('UPDATE estatus SET titulado = $1, inicio_titulacion = $2, fin_titulacion = $3, calificacion =  $4, id_mod1 = $5 WHERE id_estatus = $6',
     [graduated, startDate, endDate, average, mod, id_estatus1 ])
     .then(() => {
